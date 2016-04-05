21e2a4eeca3bfd73168deea3ae3a5896701b9a9b
Peter Park <pjh0426@gmail.com>
2016-04-05 14:08:59 -0400

    first to do

--- app/js/main.js
+++ app/js/main.js
@@ -2,3 +2,6 @@
  * Created by peterparker on 16-04-05.
  */
 alert('welcome');
+//to do
+//add a javascript code that creates paragraphs with the words in the array array1 in the div with id = div1
+var array1 = ['Apple','Orange','Banana','Melon'];
