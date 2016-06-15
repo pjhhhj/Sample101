aabee1157a8605c0e58eda30d13b339907392ca3
Peter Park <pjh0426@gmail.com>
2016-06-15 09:30:42 -0400

    Practice1

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,22 @@ alert('welcome');
 //to do
 //add a javascript code that creates paragraphs with the words in the array array1 in the div with id = div1
 var array1 = ['Apple','Orange','Banana','Melon'];
+var returnFruitNamesWithO = function(sampleArray){
+  for(var i=0; i<4; i++){
+    if(sampleArray[i] == 'Apple'){
+      alert(sampleArray[i]);
+    }
+  }
+}
+
+returnFruitNamesWithO(array1);
+
+//ToDo1:
+//Create a function that returns fruit names that starts with 'A' or 'B' only
+
+//ToDo2:
+//Create a function that outputs the first two characters of each fruit names
+
+//ToDo3:
+//Create two arrays
+//Push fruit names that start with A-H into the first array and the rest into the other array
